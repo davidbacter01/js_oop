ea50c5104398e6a085809f17742b28596da5507b
davidbacter01 <davidbacter01@gmail.com>
2024-03-20 18:56:36 +0200

    Add basic class and object examples

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('hello');
+class Product {
+    constructor(productName, price, stockQuantity) {
+        this.productName = productName;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+}
+
+const product1 = new Product('Bicicleta', 750, 1000);
+
+const product2 = {
+    productName: 'Bicicleta',
+    price: 750,
+    stockQuantity: 1000
+}
+
+const product3 = new Product('Laptop', 2500, 300);
+
+console.log(product1);
+
+console.log(product2);
+
+console.log(product3);
